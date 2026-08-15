2b2adb23bcce32eb4a6f2d8933d05fa1ac92469c
jan <jandry.rodriguez@outlook.com>
2026-08-15 20:20:47 +0100

    chore/build: create the config file for the plaid credentials to be loaded in from .env file.

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PLAID_CLIENT_ID: Optional[str] = None # Typing added to remove error suggestion in IDE
+    PLAID_SECRET: Optional[str] = None
+    PLAID_ENV: str = "sandbox"
+    REDIS_URL: str = "redis://localhost:6379/0"
+
+    # Automatically read from a local .env file
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+# Create a single reusable instance
+settings = Settings()
