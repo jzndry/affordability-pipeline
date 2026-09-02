4e38198c9e14312fbe3f6844b3b3a07f13e0d84f
jan <jandry.rodriguez@outlook.com>
2026-09-02 14:14:38 +0100

    chore: takeaway some of the boilerplate from vite startup

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
@@ -11,12 +10,10 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1></h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
